6dd3159281adb2965ed29a67708b06d0ee0ce30e
xunrongl <lixunrong@gmail.com>
2015-12-07 12:13:10 -0800

    remove debug text

--- omlet-location-share/webapps/location_share/static/location_share/js/map.js
+++ omlet-location-share/webapps/location_share/static/location_share/js/map.js
@@ -1,11 +1,27 @@
-var mapUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBcXmdCcC6w1sMcyoFfXT5fSZOLulLiCow";
+var apiKey = "";
+var mapUrl = "https://maps.googleapis.com/maps/api/js?key=" + apiKey;
 //global map object
 var map = {};
-//global markers list
+
+/*
+	store in google map markers format
+*/
 var markers = [];
+/*
+	structure like following, storing into db
+	{
+      "lat": null,
+      "timestamp": "2015-10-27T18:01:29.599351+00:00",
+      "lng": null,
+      "creation_time": "2015-10-27T18:01:29.599325+00:00",
+      "omlet_id": "kenny"
+    }
+*/
 var positions = [];
+
 //current position
 var myLatLng = {};
+
 //user account info
 var account = {};
 //group list info
@@ -13,6 +29,8 @@ var group = {
 	id: "",
 	list: []
 };
+
+var debug_text = "";
 
 var init = true;
 // loadMapAPIs();
@@ -22,58 +40,51 @@ $.getScript("https://www.google.com/jsapi", function(data, textStatus, jqXHR){
 		google.load('maps', '3', { other_params: 'key=AIzaSyBcXmdCcC6w1sMcyoFfXT5fSZOLulLiCow', 
 			callback: function() {
 		        map = new google.maps.Map(document.getElementById('map'), {
-				zoom:15
+				zoom:20
 			});
 
 			Omlet.ready(function() {
 				//get group info
 				//for local testing
 				
+				/*
+				account = {
+					name: "Ricky",
+					account: "Ricky123"
+				};
+				group.id = "Feed123";
+				group.list = [
+					{
+						displayname: "Ricky",
+						account: "Ricky123"
+					},
+					{
+						displayname: "Darren",
+						account: "Darren123"
+					}
+				]
+				*/
 				
-				// account = {
-				// 	name: "Ricky",
-				// 	account: "Ricky123"
-				// };
-				// group.id = "Feed123";
-				// group.list = [
-				// 	{
-				// 		displayname: "Ricky",
-				// 		account: "Ricky123"
-				// 	},
-				// 	{
-				// 		displayname: "Darren",
-				// 		account: "Darren123"
-				// 	}
-				// ]
-				
-
 				account = Omlet.getIdentity();		
 				group.id = Omlet.scope.feed_key;
 				//remove equal sign 
 				group.id = group.id.replace(/\=/g, '');
 
-				var text = 'omlet_id:' + account.account
-						+ 'omlet_name:' + account.name
-						+ 'group_id:' + group.id;
-				$("#name").text(text);
-				console.log(group.id);
-
-				group.list = Omlet.getFeedMembers();	
+				group.list = Omlet.getFeedMembers();
 				
-
-				uploadGroupInfo();					
+				checkGroup(group.id);					
 				//get current location
 				getLocation();
 
 				//get all markers position
 				getAllMarkers();
 				
-				// setInterval(function() {				
-				// 	//get current location
-				// 	getLocation();
-				// 	//get all markers position
-				// 	getAllMarkers();				
-				// }, 2000);			
+				setInterval(function() {					
+					//get current location
+					watchLocation();
+					//get all markers position
+					getAllMarkers();				
+				}, 7000);			
 			})
 	    }});
 					
@@ -82,86 +93,91 @@ $.getScript("https://www.google.com/jsapi", function(data, textStatus, jqXHR){
 	};
 })	
 
+//check group info exsited or not
+function checkGroup(groupID) {
+	$.ajax({
+		url: '/group/' + groupID,
+		type: 'GET',
+    	contentType: 'application/json'
+	})
+	.done(function(data) {
+		
+	})
+	.fail(function(err){
+		uploadGroupInfo();
+	});
+}
+
+
 function getLocation() {
+	
     if (navigator.geolocation) {
+    	
         navigator.geolocation.getCurrentPosition(function(position) {
+        	
         	//self location
 			myLatLng = {
 				lat: position.coords.latitude,
 				lng: position.coords.longitude
 			};
-			
 
-			if (positions.length == 1) {
-				initMarkers(positions);	
-			};
-			
-			updateLocation(myLatLng);
+			uploadLocation(myLatLng);
+        }, function(error) {        
+        	alert(error);
         });
     } else {
-        console.log("Geolocation is not supported by this browser.");
+        alert("Geolocation is not supported by this browser.");       
     }
 }
 
-function initMarkers(positions) {
+function watchLocation() {
 	
-	if (positions.length == 0) {
-		//init
-		
-		var myMarker = new google.maps.Marker({
-			position: {
-				lat: myLatLng.lat, 
-				lng: myLatLng.lng
-			},
-			map: map,
-			label: account.name.charAt(0)
-		});
-		//show users on the map
-		myMarker.setMap(map);	
-		markers.push(myMarker);
-		positions.push(myLatLng);
-	} else {
-		//remove the exsiting element
-		console.log("length is " + positions.length)
-		markers.pop();
-		//show all positions on the map
-	  	for (var i = 0; i < positions.length; i++) {
-	  		if (positions[i].lat) {
-	  			var marker = new google.maps.Marker({
-				    position: {
-				    	lat: positions[i].lat, 
-				    	lng: positions[i].lng},
-				    map: map,
-				    label: positions[i].omlet_id.charAt(0)
-				});
-				marker.setMap(map);
-				markers.push(marker);	
-	  		} else {
-	  			continue;
-	  		};	  		
-		}	
-	};
-	
-	//update the center of the map
-	updateCenter(positions);
+	navigator.geolocation.watchPosition(function(position) {
+    	//self location    
+		myLatLng = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		};
+
+		uploadLocation(myLatLng);
+    }, function(error) {
+    	alert(error);
+    });
 }
 
-function updateMarkers(positions) {
+function initMarkers(pos) {
 
-	console.log("updating the markers")
+	//show all pos on the map
+  	for (var i = 0; i < pos.length; i++) {
+  		if (pos[i].lat) {
+  			var marker = new google.maps.Marker({
+			    position: {
+			    	lat: pos[i].lat, 
+			    	lng: pos[i].lng},
+			    map: map,
+			    label: pos[i].omlet_id.charAt(0)
+			});
+			marker.setMap(map);
+			markers.push(marker);	
+  		} 	  		
+	}	
+}
+
+//update markers on the client based on the return data from the server
+function updateMarkers(pos) {
+
 	for (var i = markers.length - 1; i >= 0; i--) {
 		var postion = {
-			lat: positions[i].lat,
-			lng: positions[i].lng
-			// omlet_id: account.name
+			lat: pos[i].lat,
+			lng: pos[i].lng		
 		}
-		console.log(postion);
 		markers[i].setPosition(postion);
 	};
 }
 
-function updateLocation(position) {
-	//send the location and Omlet info to the db
+//upload the location and Omlet info to the db
+function uploadLocation(position) {
+
 	$.ajax({
 		url: '/marker/' + account.account,
 		type: 'POST',
@@ -169,10 +185,10 @@ function updateLocation(position) {
 		data: JSON.stringify(position),
 	})
 	.done(function() {
-		console.log("upload my location");
+	
 	})
 	.fail(function(err) {
-		console.log(err);
+		alert(err);
 	})
 }
 
@@ -184,31 +200,47 @@ function getAllMarkers() {
     	contentType: 'application/json'
 	})
 	.done(function(data) {
-		//update all markers
-		positions = data.markers;
-		// positions.forEach(function(marker) {
-		// 	lat: marker.lat,
-		// 	lng: marker.lng
-		// })
-		if (init) {
+
+		//get valid positions from db
+		var count = 0;
+		var init = false;
+		if (positions.length == 0) {
+			init = true;
+		} else {
+			//clear the exsited positions
+			positions =[];
+		};		
+		for (var i = data.markers.length - 1; i >= 0; i--) {
+			//only store valid positions
+			if(data.markers[i].lat && data.markers[i].lng) {
+				positions.push(data.markers[i]);			
+				count++;
+			}
+		}
+
+		if (init || positions.length < count) {
+			//initialize or new data added
 			initMarkers(positions);
+		} else {
+			//only update client's own location		
+			updateMarkers(positions);
 		};
-		updateMarkers(positions);
+		
 		updateCenter(positions);
 	});
 
 }
 
 //upload group info to db
-function uploadGroupInfo(callback) {
-	var markers = [];
+function uploadGroupInfo() {
+	var markersName = [];
 	group.list.forEach(function(member) {
-		markers.push(member.account);
+		markersName.push(member.account);
 	})
 	var data = {
 		"conversation_id": group.id,
 		"title": account.name + "'s Location",
-		"markers": markers
+		"markers": markersName
 	}
 	$.ajax({
 		url: '/group',
@@ -216,20 +248,19 @@ function uploadGroupInfo(callback) {
 		data: JSON.stringify(data),
     	contentType: 'application/json'
 	})
-	.done(callback)
+	.done()
 	.fail(function() {
 		console.log("error");
 	})
 	
 }
 
-function updateCenter(positions) {
+function updateCenter(pos) {
 	var bound = new google.maps.LatLngBounds(); 
-	for (i = 0; i < positions.length; i++) {
-		if (positions[i].lat && positions[i].lng) {
-			bound.extend(new google.maps.LatLng(positions[i].lat, positions[i].lng)); 	
+	for (i = 0; i < pos.length; i++) {
+		if (pos[i].lat && pos[i].lng) {
+			bound.extend(new google.maps.LatLng(pos[i].lat, pos[i].lng)); 	
 		}; 			
 	}
-	
 	map.setCenter(bound.getCenter());
 }
